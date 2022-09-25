2164dacae14b1a8407b95930329fbcd6e8368045
seongwon-gitcodes <ysw991106@gmail.com>
2022-09-25 17:53:42 +0900

    postgres 연결

--- nestjs-board-app/src/app.module.ts
+++ nestjs-board-app/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BoardModule } from './board/board.module';
+import { typeOrmConfig } from './configs/typeorm.config';
 
 @Module({
-  imports: [BoardModule],
+  imports: [
+    BoardModule,
+    TypeOrmModule.forRoot(typeOrmConfig)
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
